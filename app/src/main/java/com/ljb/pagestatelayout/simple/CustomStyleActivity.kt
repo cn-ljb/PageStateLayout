6ef54fb74a29c0d77b26e2e3fa1d69c3475003bb
cn-ljb <cn_ljb@sina.cn>
2018-09-03 00:07:11 +0800

    first commit

---
+++ app/src/main/java/com/ljb/pagestatelayout/simple/CustomStyleActivity.kt
@@ -0,0 +1,57 @@
+package com.ljb.pagestatelayout.simple
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import com.ljb.page.PageStateLayout
+import com.ljb.pagestatelayout.simple.utils.RxUtils
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_custom_style.*
+import java.util.concurrent.TimeUnit
+
+/**
+ * Author:Ljb
+ * Time:2018/9/2
+ * There is a lot of misery in life
+ **/
+class CustomStyleActivity : Activity() {
+
+    private var mDemoDisposable: Disposable? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_style)
+        initTask()
+    }
+
+    private fun initTask() {
+        mDemoDisposable = Observable
+                .interval(0, 3000, TimeUnit.MILLISECONDS)
+                .map { it % 6 }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { changePage(it.toInt()) }
+    }
+
+    private fun changePage(num: Int) {
+        Log.i("pageState", "num = $num")
+        val pageState = if (num % 2 == 0) {
+            PageStateLayout.PageState.STATE_LOADING
+        } else {
+            when (num) {
+                1 -> PageStateLayout.PageState.STATE_SUCCESS
+                3 -> PageStateLayout.PageState.STATE_EMPTY
+                5 -> PageStateLayout.PageState.STATE_ERROR
+                else -> PageStateLayout.PageState.STATE_LOADING
+            }
+        }
+        page_layout.setPage(pageState)
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        RxUtils.dispose(mDemoDisposable)
+    }
+}
---
+++ app/src/main/java/com/ljb/pagestatelayout/simple/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.ljb.pagestatelayout.simple
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn_def_style.setOnClickListener { goDefStylePage() }
+        btn_custom_style.setOnClickListener { goCustomStylePage() }
+    }
+
+    private fun goCustomStylePage() {
+        startActivity(Intent(this, CustomStyleActivity::class.java))
+    }
+
+    private fun goDefStylePage() {
+        startActivity(Intent(this, DefStyleActivity::class.java))
+    }
+}
